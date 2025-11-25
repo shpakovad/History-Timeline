ee41788321888423b9367b0baae240856d706102
shpakovad <shpakovad@gmail.com>
2025-11-25 14:54:29 +0300

    Added circle background into swiper buttons.Added useDevice hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
-import { RefObject, useRef } from "react";
 
 import HistoryTimeline from "./components/HistoryTimeline/HistoryTimeline";
 import CrossLayout from "./components/layouts/CrossLayout/CrossLayout";
-import {useDeviceDetection} from "@/hooks/useDeviceDetection";
 
 const App = () => {
-  const {isMobile} = useDeviceDetection();
+
   return (
     <div>
       <CrossLayout>
