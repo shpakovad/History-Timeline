879e98ea4351e413b7f6b40346875647b72fe9bf
shpakovad <shpakovad@gmail.com>
2025-11-27 13:54:52 +0300

    Fixed webpack

--- webpack.config.ts
+++ webpack.config.ts
@@ -66,7 +66,7 @@ const config: Configuration = {
       new TerserPlugin({
         terserOptions: {
           compress: {
-            drop_console: true,
+            drop_console: false,
           }
         }
       }),
